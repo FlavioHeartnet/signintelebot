c4993d8187321080604742a94ba1bd10cf2cade0
Flavio <flavionogueirabarros@gmail.com>
2024-12-12 17:31:56 -0300

    add get parameters

--- src/app/api/OAuth/route.ts
+++ src/app/api/OAuth/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    console.log(request);
+    console.log(request.nextUrl.searchParams);
     return NextResponse.json({ message: request.body }, { status: 200 });
 }
