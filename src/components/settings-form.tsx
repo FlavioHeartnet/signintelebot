dadbc484460f7960fc7e45450ec6c06b3052d224
Flavio <flavionogueirabarros@gmail.com>
2024-12-13 18:34:18 -0300

    add new ico

--- src/components/settings-form.tsx
+++ src/components/settings-form.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Lock} from "lucide-react";
+import { Lock } from "lucide-react";
 
 export default function SettingsForm() {
   return (
