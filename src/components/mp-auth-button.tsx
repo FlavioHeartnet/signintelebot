c6c9e6b354784aab1ba2f71abe33d8a30e3db24e
Flavio <flavionogueirabarros@gmail.com>
2024-12-13 23:39:34 -0300

    change ngrok url

--- src/components/mp-auth-button.tsx
+++ src/components/mp-auth-button.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MercadoPagoAuthButton() {
   const link =
-    "https://auth.mercadopago.com.br/authorization?client_id=7463974713567328&response_type=code&platform_id=mp&redirect_uri=https://7582-2804-14d-baa7-83a7-3093-ee9-bbb1-994.ngrok-free.app/OAuth";
+    "https://auth.mercadopago.com.br/authorization?client_id=7463974713567328&response_type=code&platform_id=mp&redirect_uri=https://sweet-dog-wanted.ngrok-free.app/OAuth";
   return (
     <Link
       href={link}
